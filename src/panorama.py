6decbc08ab9a71ceef42a28589b3d915feaede99
daniel <daniel.segal3@mail.huji.ac.il>
2020-09-01 12:34:07 +0300

    changed gradient computation - now returns a tuple

--- src/panorama.py
+++ src/panorama.py
@@ -4,7 +4,7 @@ from scipy.signal import convolve2d
 from scipy.ndimage import gaussian_filter
 
 """
-returns the magnitude of gradient of image
+returns the magnitude of gradient of image, and the derivatives dx, dy
 """
 def comp_grads(im):
     dx = np.array([[-1, 0, 1]])
@@ -14,7 +14,7 @@ def comp_grads(im):
     dim_x = convolve2d(im, dx, mode="same")
     dim_y = convolve2d(im, dy, mode="same")
 
-    return np.sqrt(dim_x**2 + dim_y**2)
+    return dx, dy, np.sqrt(dim_x**2 + dim_y**2)
 
 
 
